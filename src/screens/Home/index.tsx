389688c374f5edcf326a6773bd8177703499b2d8
danielmottaa <dev.danielmotta@gmail.com>
2022-12-02 10:30:43 -0300

    :rocket: adjustment repositorie

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,11 @@
-import React, {Fragment, useEffect, useState} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import React, { Fragment, useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import HorizontalLine from '../../shared/HorizontalLine';
 import Input from '../../shared/Input';
 import api from '../../services/api';
 import FooterList from '../../shared/FooterList';
-import {useDispatch} from 'react-redux';
-import {changeRouteName} from '../../store/ducks/info';
+import { useDispatch } from 'react-redux';
+import { changeRouteName } from '../../store/ducks/info';
 import * as S from './styles';
 
 const Home: React.FC = () => {
@@ -14,19 +14,18 @@ const Home: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [filteredData, setFilteredData] = useState([]);
-  const {navigate} = useNavigation<any>();
+  const { navigate } = useNavigation<any>();
   const dispatch = useDispatch();
   const perPage = 15;
 
   const getInfoUserGithub = async () => {
     try {
       setLoading(true);
-      if (filteredData) {
-        const response = await api.get(
-          `/search/repositories?q=react-native&per_page=${perPage}&page=${page}`,
-        );
-        setData([...data, ...response.data.items]);
-      }
+      const response = await api.get(
+        `/search/repositories?q=diego3g&per_page=${perPage}&page=${page}`,
+      );
+      setData([...data, ...response.data.items]);
+      if (!!data.length) setFilteredData(data);
       setLoading(false);
     } catch (error) {
       console.log(error);
@@ -41,67 +40,62 @@ const Home: React.FC = () => {
     getInfoUserGithub();
   }, [page]);
 
-  useEffect(() => {
-    if (!!data.length) {
-      setFilteredData(data);
-    }
-  }, [data]);
 
-  const renderItem = ({item}: any) => {
-    return (
-      <Fragment key={item}>
-        <S.ContainerContent>
-          <S.ContentInLine
-            onPress={() => {
-              dispatch(changeRouteName(item.name));
-              navigate('Internal', {item});
-            }}>
-            <S.ImageCustom source={{uri: item?.owner?.avatar_url}} />
-            <S.BoxNameRepoAndOwner>
-              <S.NameRepo>{item.name}</S.NameRepo>
-              <S.NameOwner>{item?.owner?.login}</S.NameOwner>
-            </S.BoxNameRepoAndOwner>
-            <S.NumberOfStars>{item.stargazers_count} stars</S.NumberOfStars>
-          </S.ContentInLine>
-        </S.ContainerContent>
-        <HorizontalLine />
-      </Fragment>
-    );
-  };
-
+const renderItem = ({ item }: any) => {
   return (
-    <S.Container>
-      <S.TitleHome>Repositórios</S.TitleHome>
-      <S.BoxInput>
-        <Input
-          placeholder={'Busca por repositórios'}
-          keyboardType={'default'}
-          value={searchData}
-          onChangeText={text => setSearchData(text)}
-          onSubmitEditing={() => {
-            const filterData = data?.filter((item: any) => {
-              return (
-                item?.name?.toLowerCase().indexOf(searchData.toLowerCase()) >= 0
-              );
-            });
-            setFilteredData(filterData);
-          }}
-        />
-      </S.BoxInput>
-      <S.BoxFlatlist>
-        <S.FlatListCustom
-          data={filteredData}
-          renderItem={renderItem}
-          keyExtractor={(item, index) => String(index)}
-          showsVerticalScrollIndicator={false}
-          removeClippedSubviews={true}
-          onEndReached={loadMoreItem}
-          onEndReachedThreshold={0.1}
-          ListFooterComponent={<FooterList load={loading} />}
-        />
-      </S.BoxFlatlist>
-    </S.Container>
+    <Fragment key={item}>
+      <S.ContainerContent>
+        <S.ContentInLine
+          onPress={() => {
+            dispatch(changeRouteName(item.name));
+            navigate('Internal', { item });
+          }}>
+          <S.ImageCustom source={{ uri: item?.owner?.avatar_url }} />
+          <S.BoxNameRepoAndOwner>
+            <S.NameRepo>{item.name}</S.NameRepo>
+            <S.NameOwner>{item?.owner?.login}</S.NameOwner>
+          </S.BoxNameRepoAndOwner>
+          <S.NumberOfStars>{item.stargazers_count} stars</S.NumberOfStars>
+        </S.ContentInLine>
+      </S.ContainerContent>
+      <HorizontalLine />
+    </Fragment>
   );
+};
+
+return (
+  <S.Container>
+    <S.TitleHome>Repositórios</S.TitleHome>
+    <S.BoxInput>
+      <Input
+        placeholder={'Busca por repositórios'}
+        keyboardType={'default'}
+        value={searchData}
+        onChangeText={text => setSearchData(text)}
+        onSubmitEditing={() => {
+          const filterData = data?.filter((item: any) => {
+            return (
+              item?.name?.toLowerCase().indexOf(searchData.toLowerCase()) >= 0
+            );
+          });
+          setFilteredData(filterData);
+        }}
+      />
+    </S.BoxInput>
+    <S.BoxFlatlist>
+      <S.FlatListCustom
+        data={filteredData}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => String(index)}
+        showsVerticalScrollIndicator={false}
+        removeClippedSubviews={true}
+        onEndReached={loadMoreItem}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={<FooterList load={loading} />}
+      />
+    </S.BoxFlatlist>
+  </S.Container>
+);
 };
 
 export default Home;
